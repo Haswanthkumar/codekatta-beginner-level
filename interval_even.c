f4bfeb1bddcdef678ce94c1001456633fa00cf88
HK-1427 <34661540+Haswanthkumar@users.noreply.github.com>
2017-12-22 14:02:00 +0530

    Create interval_even.c

---
+++ interval_even.c
@@ -0,0 +1,18 @@
+#include<Stdio.h>
+int main(void)
+{
+    int s,e,i;
+    printf("enter the starting number\n");
+    scanf("%d",&s);
+    printf("enter the ending number\n");
+    scanf("%d",&e);
+    s=s+1;
+    for(i=s;i<=e;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+return 0;
+}
